cdf69de2960bb2dc2c3e65c0b4a4b27ef4f116e9
Jamest011 <Jamest011@byui.edu>
2022-05-22 20:37:41 -0500

    updated CSS and layout to match requirements for the assignment.

--- chamber/scripts/jscripts.js
+++ chamber/scripts/jscripts.js
@@ -4,15 +4,22 @@ document.getElementById("currentYear").innerHTML = new Date().getFullYear();
 document.getElementById("lastUpdated").innerHTML = document.lastModified;
 document.getElementById("currentDate").innerHTML = new Date().toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"long", day:"numeric"});
 
-// hamburger button info
+// hamburger button
 
 function toggleMenu() {
-  document.getElementById('navigation').classList.toggle('open');
+  document.getElementById('mainNav').classList.toggle('open');
+  document.getElementById('menuButton').classList.toggle('open');
 }
-const x = document.getElementById('hamburgerBtn');
+const x = document.getElementById('menuButton');
 x.onclick = toggleMenu;
 
 // Mon and Tues banner
 if (now.getDay() === 1 || now.getDay() === 2) {
   document.getElementById("banner").style.display = "block";
 }
+
+const close = document.querySelector("#close");
+
+close.addEventListener("click", () => {
+    banner.style.display = "none";
+});
