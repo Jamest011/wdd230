bd48216602a6bd0102662350668f9cb3c0b55515
Jamest011 <Jamest011@byui.edu>
2022-06-03 01:36:13 -0500

    fix

--- lesson7/scripts/lazy_load.js
+++ lesson7/scripts/lazy_load.js
@@ -4,7 +4,7 @@ const imagesToLoad = document.querySelectorAll("img[data-src]");
 // parameters set for intersectionalObserver
 const imgOptions = {
     threshold: 1,
-    rootMargin: "0px 0px -50px 0px"
+    rootMargin: "0px 0px -20px 0px"
 };
 
 const loadImages = (image) => {
