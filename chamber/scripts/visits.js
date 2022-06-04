8678fc6ad69b857eff792d377735d27528f74b5c
Jamest011 <Jamest011@byui.edu>
2022-06-04 16:31:45 -0500

    js code update

--- chamber/scripts/visits.js
+++ chamber/scripts/visits.js
@@ -3,6 +3,7 @@ const visitsDisplay = document.querySelector(".visits");
 
 // get the stored value in localStorage
 let numVisits = Number(window.localStorage.getItem("visits-ls"));
+let numMills = Number(window.localStorage.getItem("mills-ls"));
 
 // first visit or number to display
 if (numVisits !== 0) {
@@ -17,3 +18,16 @@ numVisits++;
 
 // store the new number of visits
 localStorage.setItem("visits-ls", numVisits);
+
+// show number of days
+// days since last visit
+if (numMills !== 0) {
+    numDays =Math.round((Date.now() - numMills) / 86400000);
+    todayDisplay.textContent = numDays;
+}
+else {
+    todayDisplay.textContent = 'This is your first visit!';
+}
+
+numMills = Date.now();
+localStorage.setItem("mills-ls", numMills);
